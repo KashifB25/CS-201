d8e352a4d6a1f4bb148989387b1c28ed67fc756a
KashifB25 <75171453+KashifB25@users.noreply.github.com>
2021-10-13 21:24:00 +0530

    Complete code

--- Assignment 1/Q1/1.c
+++ Assignment 1/Q1/1.c
@@ -1,6 +1,35 @@
 #include <stdio.h>
+#include <string.h>
+
+int isValidFloat(char *s);
 
 int main(void)
 {
-  
-}  
+    char num[50];
+
+    scanf("%s", num);
+
+    if(isValidFloat(num))
+    {
+        printf("valid");
+    }
+    else{
+        printf("invalid");
+    }
+
+    return 0;
+}
+
+int isValidFloat(char *s)
+{
+    int isValid = 0;
+    for (int i = 0, n = strlen(s); i < n; i++)
+    {
+        if (s[i] == '.' && s[i+1] != 0)
+        {
+            isValid = 1;
+        }
+    }
+    return isValid;
+
+}
