1bbb11d85c0adcf14bce9d1b32ec39434138206b
KashifB25 <75171453+KashifB25@users.noreply.github.com>
2021-10-18 00:56:30 +0530

    Update code

--- Assignment 1/Q3/3.c
+++ Assignment 1/Q3/3.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 
-int main(void)
+int main(void) 
 {
+  int n, m;
+  scanf("%d %d", &n, &m);
+  
+  int arrayOneD[n*m];
+  int arrayTwoD[n][m];
+  for(int i = 0; i<n*m; i++)
+    scanf("%d", &arrayOneD[i]);
 
-}  
+  for(int i = 0; i<n; i++)
+    for(int j = 0; j<m; j++)
+      arrayTwoD[i][j] = arrayOneD[i*m + j];
+
+  for(int i = 0; i<n; i++) 
+  {
+    for(int j = 0; j<m; j++)
+      printf("%d ", arrayTwoD[i][j]);
+      printf("\n");
+  }
+  return 0;
+}
