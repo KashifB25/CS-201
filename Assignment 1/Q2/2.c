888c563b2377ce15dc0e1c39d964733d34b2f065
KashifB25 <75171453+KashifB25@users.noreply.github.com>
2021-10-17 19:29:11 +0530

    Update code

--- Assignment 1/Q2/2.c
+++ Assignment 1/Q2/2.c
@@ -1,6 +1,59 @@
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+
+int isValidEmail(char *s);
 
 int main(void)
 {
+    char email[50];
+    scanf("%s", email);
   
-}  
+    if(isValidEmail(email))
+    {
+        printf("valid");
+    }
+    else
+    {
+        printf("invalid");
+    }
+  
+    return 0;
+
+}
+
+int isValidEmail(char *s)
+{
+    if(!isalpha(s[0]))
+    {
+        return 0;
+    }
+
+    int atPosition = -1;
+    int dotPosition = -1;
+
+    for (int i = 0, n =strlen(s); i < n; i++)
+    {
+        if(s[i] == '@')
+        {
+            atPosition = i;
+        }
+        else if(s[i] == '.')
+        {
+            dotPosition = i;
+        }
+    }
+
+    if (atPosition == -1 || dotPosition == -1)
+    {
+        return 0;
+    }
+
+    if(atPosition > dotPosition)
+    {
+        return 0;
+    }
+
+    return !(dotPosition >= (strlen(s) - 1));
+}
